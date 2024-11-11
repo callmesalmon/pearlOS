4b748f0bfa112fb2509dcac8691803fdb35e9eb8
eliss <staaf.elis@gmail.com>
2024-11-11 14:33:15 +0100

    Add better message when booting up kernel
    [src/impl/kernel/main.c] =>
    Much, much better message. There's even a title
    <https://patorjk.com/software/taag> made out of ASCII:
    
      /$$$$$$   /$$$$$$  /$$
     /$$__  $$ /$$__  $$| $$
    | $$  \ $$| $$  \__/| $$   /$$
    | $$  | $$|  $$$$$$ | $$  /$$/
    | $$  | $$ \____  $$| $$$$$$/
    | $$  | $$ /$$  \ $$| $$_  $$
    |  $$$$$$/|  $$$$$$/| $$ \  $$
     \______/  \______/ |__/  \__/
    
    Looks pretty good? As I'm writing this in my terminal it looks great,
    but maybe on github this looks like shit.

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,7 +1,19 @@
-#include "print.h"
+#include "../../intf/print.h"
 
 void kernel_main() {
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("Welcome to our 64-bit kernel!");
+    print_str(
+    "  /$$$$$$   /$$$$$$  /$$      \n"
+    " /$$__  $$ /$$__  $$| $$      \n"
+    "| $$  \\ $$| $$  \\__/| $$   /$$\n"
+    "| $$  | $$|  $$$$$$ | $$  /$$/\n"
+    "| $$  | $$ \\____  $$| $$$$$$/ \n"
+    "| $$  | $$ /$$  \\ $$| $$_  $$ \n"
+    "|  $$$$$$/|  $$$$$$/| $$ \\  $$\n"
+    "\\______/  \\______/ |__/  \\__/ \n"
+    "\n"
+    "Hello World! Welcome to the small OSk\n"
+    "kernel! It... Works? Yeah! So enjoy!\n"
+    );
 }
