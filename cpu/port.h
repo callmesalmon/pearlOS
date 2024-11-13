4f2057153031b033e46252fe49329c47ea694be7
eliss <staaf.elis@gmail.com>
2024-11-13 22:09:16 +0100

    stupid space x2

--- cpu/port.h
+++ cpu/port.h
@@ -2,7 +2,7 @@
 
 #include "../lib/type.h"
 
-byte port_byte_in (word port);
-void port_byte_out (word port, byte data);
-word port_word_in (word port);
-void port_word_out (word port, word data);
+byte port_byte_in(word port);
+void port_byte_out(word port, byte data);
+word port_word_in(word port);
+void port_word_out(word port, word data);
