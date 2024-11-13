06e56d30b697c3cfb129546634964115fec7bff5
Elis Staaf <staaf.elis@gmail.com>
2024-11-13 17:28:56 +0100

    INVALID AAAAAAAAa

--- tools/archive.sh
+++ tools/archive.sh
@@ -1,4 +1,4 @@
 echo "Making zip image..."
-zip dist/pearl-os-image.zip dist/os-image.bin
+zip dist/pearlOS.zip dist/os-image.bin
 echo "Making tar image..."
-tar -cvf dist/pearl-os-image.tar dist/os-image.bin
+tar -cvf dist/pearlOS.tar dist/os-image.bin
