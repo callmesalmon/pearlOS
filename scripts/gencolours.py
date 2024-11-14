aa7cf1bc5b6264f63ec610b39b9a75bd9b1100e0
eliss <staaf.elis@gmail.com>
2024-11-14 23:07:48 +0100

    fmt

--- scripts/gencolours.py
+++ scripts/gencolours.py
@@ -2,22 +2,22 @@
 import sys
 
 colors = {
-    "BLACK" : "0",
-    "BLUE" : "1",
-    "GREEN" : "2",
-    "CYAN" : "3",
-    "RED" : "4",
-    "PURPLE" : "5",
-    "BROWN" : "6",
-    "GRAY" : "7",
-    "DARK_GRAY" : "8",
-    "LIGHT_BLUE" : "9",
-    "LIGHT_GREEN" : "a",
-    "LIGHT_CYAN" : "b",
-    "LIGHT_RED" : "c",
+    "BLACK"        : "0",
+    "BLUE"         : "1",
+    "GREEN"        : "2",
+    "CYAN"         : "3",
+    "RED"          : "4",
+    "PURPLE"       : "5",
+    "BROWN"        : "6",
+    "GRAY"         : "7",
+    "DARK_GRAY"    : "8",
+    "LIGHT_BLUE"   : "9",
+    "LIGHT_GREEN"  : "a",
+    "LIGHT_CYAN"   : "b",
+    "LIGHT_RED"    : "c",
     "LIGHT_PURPLE" : "d",
-    "YELLOW" : "e",
-    "WHITE" : "f"
+    "YELLOW"       : "e",
+    "WHITE"        : "f"
 }
 
 def generate():
@@ -32,13 +32,13 @@ def generate():
                 result += "#define " + text_color + "_ON_" + background_color + " 0x" + background_code + text_code + "\n"
     return result
 
-def wf(filename, data):
+def fwrite(filename, data):
     with open(filename, "w") as f:
         return f.write(str(data))   # str() to make sure
 
 def main():
     if len(sys.argv) > 1:
-        wf(sys.argv[1], generate())
+        fwrite(sys.argv[1], generate())
     else:
         print(generate())
 
