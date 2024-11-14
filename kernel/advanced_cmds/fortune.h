a9633c1997c024408a18e26e2ae86e268aea928c
eliss <staaf.elis@gmail.com>
2024-11-14 18:46:27 +0100

    Add some more fortunes
    kernel/advanced_cmds/fortune.h
        Add some more fortunes to this operating systems fortune cookie.

--- kernel/advanced_cmds/fortune.h
+++ kernel/advanced_cmds/fortune.h
@@ -5,12 +5,43 @@
 #include "../../drivers/display.h"
 
 void ksh_fortune() {
-  const char *fortunes[] = {
-      "Pohl's Law: Nothing is so good that somebody, somewhere, will not hate it.", 
-      "You either die a smart fella, or live long enough to become a fart smella",
-      "Everyone asked you about your favorite dinosaur as a kid, now, nobody cares"};
-  kprintc('\n');
-  int RandIndex = rand() % 3;
-  kprintc(*fortunes[RandIndex]);
-  kprintc('\n');
+    const char *fortunes[] = {
+        "Pohl's Law: Nothing is so good that somebody, somewhere, will not hate it.\n",
+
+        "You either die a smart fella, or live long enough to become a fart smella\n",
+
+        "Everyone asked you about your favorite dinosaur as a kid, now, nobody cares\n",
+
+        "3rd Law of Computing:\n"
+        "    Anything that can go wr\n"
+        "fortune: Segmentation violation -- Core dumped\n",
+        
+        "Experience, n.:\n"
+	    "    Something you don't get until just after you need it.\n"
+		"        -- Olivier\n",
+
+        "Famous quotations:\n\n"
+        "    \" \"\n"
+		"       -- Charlie Chaplin\n\n"
+        "    \" \"\n"
+	    "    	-- Harpo Marx\n\n"
+        "	\" \"\n"
+	    "    	-- Marcel Marceau\n",
+
+        "Ginsberg's Theorem:\n"
+        "   (1) You can't win.\n"
+        "   (2) You can't break even.\n"
+        "   (3) You can't even quit the game.\n\n"
+        "Freeman's Commentary on Ginsberg's theorem:\n"
+        "   Every major philosophy that attempts to make life seem\n"
+        "   meaningful is based on the negation of one part of Ginsberg's\n"
+        "   Theorem.  To wit:\n\n"
+        "   (1) Capitalism is based on the assumption that you can win.\n"
+        "   (2) Socialism is based on the assumption that you can break even.\n"
+        "   (3) Mysticism is based on the assumption that you can quit the game.\n"
+        
+    };
+    kprintc('\n');
+    int RandIndex = rand() % 3;
+    kprintc(*fortunes[RandIndex]);
 }
