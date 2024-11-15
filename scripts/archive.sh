c0085502f7fa979de89cfcba4fbbb223ae3d4d74
eliss <staaf.elis@gmail.com>
2024-11-15 22:14:09 +0100

    ix

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -1,6 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Making zip image..."
-zip dist/pearlOS.zip dist/os-image.bin
-echo "Making tar image..."
-tar -cvf dist/pearlOS.tar dist/os-image.bin
+case $1 in 
+    "--zip" | "-z")
+        echo "Making zip image..."
+        zip dist/pearlOS.zip dist/os-image.bin
+        ;;
+
+    "--tar" | "-t")
+        echo "Making tar image..."
+        tar -cvf dist/pearlOS.tar dist/os-image.bin
+        ;;
+
+    *)
+        echo "Invalid option, please provide a valid flag."
+        ;;
+esac
