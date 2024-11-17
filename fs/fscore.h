648b9b8a5e102df7a39ef0430d6084d38b1f11de
eliss <staaf.elis@gmail.com>
2024-11-17 14:09:42 +0100

    stupid fscore fmt

--- fs/fscore.h
+++ fs/fscore.h
@@ -16,15 +16,15 @@
 #define FILE_NOT_FOUND          1
 
 // declarations
-void fsinit();
-int file_remove(char* name);
-int file_make(char* name);
+void  fsinit();
+int   file_remove(char* name);
+int   file_make(char* name);
 char* file_get_name(int id);
-int file_get_id(char* name);
-int file_count();
-bool file_exists(char* name);
-int file_size(char* name);
-int file_read(char* filename, char* output);
-int file_write(char* filename, char* data, uint32_t depth);
-int file_writes(char* filename, char* text);
-int file_clean(char* filename);
+int   file_get_id(char* name);
+int   file_count();
+bool  file_exists(char* name);
+int   file_size(char* name);
+int   file_read(char* filename, char* output);
+int   file_write(char* filename, char* data, uint32_t depth);
+int   file_writes(char* filename, char* text);
+int   file_clean(char* filename);
