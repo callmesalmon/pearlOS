e3c15fb5cb62b50da3950fcfd2fae5720422b03c
eliss <staaf.elis@gmail.com>
2024-11-21 19:03:31 +0100

    config comments
    ... (I'm never doing anything productive fuck you)

--- kernel/config.h
+++ kernel/config.h
@@ -1,9 +1,9 @@
-// kernel.c
+/* kernel.c */
 #define DEFAULT_THEME  WHITE_ON_BLUE
 #define KERNEL_STARTUP ksh_start();
 #define KERNEL_UPDATE
 
-// mem.h
+/* mem.h */
 #define KERNEL_MEMORY_OFFSET_START 0xffffff
 #define KERNEL_MEMORY_OFFSET_END   0xffffffff
 #define MEMORY_INDEX_BASE_SIZE     10000
