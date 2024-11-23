0617434c16d825aae22585e35907e2711257131c
eliss <staaf.elis@gmail.com>
2024-11-23 15:59:22 +0100

    Add pow() (^) to calc
    using math.pow() I added this

--- kernel/advanced_cmds/calc.h
+++ kernel/advanced_cmds/calc.h
@@ -1,32 +1,38 @@
 #include "../io.h"
+#include "../mem.h"
 #include "../../lib/string.h"
 #include "../../lib/conv.h"
-#include "../mem.h"
+#include "../../lib/math.h"
 
 #define strint(str) (str_to_int(str, (int)(sizeof str / sizeof *str)))
 
 int calc_init()
 {
     /* Define */
-    char* n1 = (char*) kmalloc(255);
-    char* n2 = (char*) kmalloc(255);
+    char* s1 = (char*) kmalloc(255);
+    char* s2 = (char*) kmalloc(255);
     char* op = (char*) kmalloc(255);
 
     /* Collect */
     kprints("num> ");
-    kinputs(n1);
+    kinputs(s1);
     kprints("num> ");
-    kinputs(n2);
+    kinputs(s2);
     kprints("op> ");
     kinputs(op);
 
     kprints("res> ");
 
+    /* Convert */
+    int n1 = strint(s1);
+    int n2 = strint(s2);
+
     /* Handle */
-    if (strcmp(op, "+")) kprintu(strint(n1) + strint(n2));
-    else if (strcmp(op, "-")) kprintu(strint(n1) - strint(n2));
-    else if (strcmp(op, "*")) kprintu(strint(n1) * strint(n2));
-    else if (strcmp(op, "/")) kprintu(strint(n1) / strint(n2));
+    if (strcmp(op, "+"))      kprintu(n1 + n2);
+    else if (strcmp(op, "-")) kprintu(n1 - n2);
+    else if (strcmp(op, "*")) kprintu(n1 * n2);
+    else if (strcmp(op, "/")) kprintu(n1 / n2);
+    else if (strcmp(op, "^")) kprintu(pow(n1, n2));
     else kprints("Invalid operator.");
 
     kprintc('\n');
