166e73c273c5ad055a886bef1399deb706507cbd
eliss <staaf.elis@gmail.com>
2024-12-01 18:25:53 +0100

    Add various mathematic constants to math.h
    PHI: 1.61
    PI:  3.14
    E:   2.71

--- lib/math.h
+++ lib/math.h
@@ -1,4 +1,7 @@
 #define INF ~(1<<31)
+#define PHI 1.61803398874989
+#define PI  3.14159265358979
+#define E   2.71828182845904
 
 double pow(double base, double exponent);
 double abs(double number);
