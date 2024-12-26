f5b1106eb36471d7314941f8fc6ab678e7a485d7
eliss <elis.staaf@proton.me>
2024-12-27 00:37:24 +0100

    Fix comments in <cpu/port.c>

--- cpu/port.c
+++ cpu/port.c
@@ -19,26 +19,26 @@ under the License.
 
 #include <cpu/port.h>
 
-// get from port
+/* Get from port */
 byte port_byte_in(word port) {
     byte result;
     __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
     return result;
 }
 
-// send to port
+/* Send to port */
 void port_byte_out(word port, byte data) {
     __asm__("out %%al, %%dx" : : "a" (data), "d" (port));
 }
 
-// get from port
+/* Get from port */
 word port_word_in(word port) {
     unsigned short result;
     __asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
     return result;
 }
 
-// send to port
+/* Send to port */
 void port_word_out(unsigned short port, word data) {
     __asm__("out %%ax, %%dx" : : "a" (data), "d" (port));
 }
