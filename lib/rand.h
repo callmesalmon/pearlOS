2d45468245e33c81f1078e9665bd89004261d530
eliss <elis.staaf@proton.me>
2024-12-27 01:06:55 +0100

    HOW I LOVE COMMENTS HAHAHA

--- lib/rand.h
+++ lib/rand.h
@@ -19,12 +19,15 @@ under the License.
 
 #pragma once
 
+/* Includes */
 #include <stddef.h>
 
+/* Defines */
 #define LCG_A 1103515245
 #define LCG_C 12345
 #define LCG_M 2147483648
 
+/* Functions */
 void rand_init();
 int rand_lcg(int x);
 int rand();
