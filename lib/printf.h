be181dbb8b7833cee89ff1c2700b8bcdccebe99a
eliss <elis.staaf@proton.me>
2025-01-02 12:41:24 +0100

    Add printf()
    This was not fun to add. The implementation ended up being so big that I
    needed to put it into a separate file from I/O. ``printf.h``. But the
    functions in printf.h all have the suffix "_", and are then turned into
    their respective counterparts in io.h:
    
        #include <printf.h>
    
        #define printf  printf_
        #define sprintf sprintf_

--- lib/printf.h
+++ lib/printf.h
@@ -22,23 +22,13 @@ under the License.
 #pragma once
 
 /* Includes */
-#include <stddef.h>
-#include <printf.h>
-#include <drivers/display.h>
-#include <drivers/keyboard.h>
-#include <drivers/display_color.h>
+#include <io.h>
 
 /* Defines */
-#define printf  printf_
-#define sprintf sprintf_
+#ifndef putchar
+#   define putchar(c) (printc(c))
+#endif
 
 /* Functions */
-void printi(int number);
-void printu(uint number);
-void printu32(uint32_t number);
-void printk(char* text);
-void printhex(uint32_t number);
-void cprintk(char* text, char color);
-void println(char* text);
-void cprintln(char* text, char color);
-void scan(char* output);
+int printf_(const char *format, ...);
+int sprintf_(char *out, const char *format, ...);
--- lib/stdarg.h
+++ lib/stdarg.h
@@ -25,7 +25,7 @@ under the License.
 typedef __builtin_va_list va_list;
 
 /* Defines */
-#define va_start(ap, ...) __builtin_va_start(ap, 0)
-#define va_end(ap) __builtin_va_end(ap)
-#define va_arg(ap, type) __builtin_va_args(ap, type)
+#define va_start(ap, ...)  __builtin_va_start(ap, 0)
+#define va_end(ap)         __builtin_va_end(ap)
+#define va_arg(ap, type)   __builtin_va_arg(ap, type)
 #define va_copy(dest, src) __builtin_va_copy(dest, src)
