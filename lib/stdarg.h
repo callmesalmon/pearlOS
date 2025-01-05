2b817b46c9d4e26b8c77bee2028ebf346f4621ea
eliss <elis.staaf@proton.me>
2025-01-05 11:54:35 +0100

    [feat] Add the sixth commandment

--- lib/stdarg.h
+++ lib/stdarg.h
@@ -21,6 +21,28 @@ under the License.
 
 #pragma once
 
+/* * * * * * * * * * * * * * * * * * *
+ *     The sixth commandment.        *
+ * * * * * * * * * * * * * * * * * * *
+ * When there is a builtin function  *
+ * achieving thy goal, thou shall    *
+ * utilize that function, e.g this   *
+ * is a sin:                         *
+ *                                   *
+ *     #define va_start(ap, ...) \   *
+ *         // Complicated impl...    *
+ *                                   *
+ * Yet thy soul shall be spared      *
+ * should thou commence with this:   *
+ *                                   *
+ *     #define va_start(ap, ...) \   *
+ *     __builtin_va_start(ap, 0)     *
+ *                                   *
+ * We DO NOT need more complicated   *
+ * implementations, MARTHA. FOR THE  *
+ * LOVE OF C AND C++!                *
+ * * * * * * * * * * * * * * * * * * */
+
 /* Types */
 typedef __builtin_va_list va_list;
 
