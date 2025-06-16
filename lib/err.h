d64fc3558bee1dee7fcffc7243f01c2784907203
salmon <elis.staaf@proton.me>
2025-06-16 23:04:56 +0200

    fix: Revamp style in ``err.h``

--- lib/err.h
+++ lib/err.h
@@ -12,16 +12,11 @@ typedef struct {
     int code;
 } excode;
 
-excode exitval = {
-        .code = 0
+excode exitval = { 
+    .code = 0 
 };
 
-excode errval = {
-    .code = 1
-};
-
-excode errval256 = {
-    .code = 256
-};
+excode errval    = { .code = 1   };
+excode errval256 = { .code = 256 };
 
 #define fetch return
