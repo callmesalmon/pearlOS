530aa65d71d170e7e011ba94ee630a98c24f58f6
salmon <elis.staaf@proton.me>
2025-06-17 12:56:38 +0200

    feat: Remove claim of any ownership of the ``Zeptex`` editor

--- zeptex/editor.h
+++ zeptex/editor.h
@@ -1,11 +1,3 @@
-/*
-* Copyright (c) Salmon 2025 under the ANTIFA-MIT license.
-* If your copy of this program doesn't include the license, it is
-* available to read at: 
-*
-*     https://github.com/jamiebuilds/anti-fascist-mit-license
-*/
-
 #pragma once
 
 #include <io.h>
