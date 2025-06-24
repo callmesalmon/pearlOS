0db4478ddffd25c27cf570098766892edf60f617
androvonx95 <androvonx95@tutamail.com>
2025-06-24 23:32:51 +0530

    Added the data structres back

---
+++ struct/stack.h
@@ -0,0 +1,63 @@
+#pragma once
+
+/* Includes */
+#include <stddef.h>
+
+/* Stack structure */
+typedef struct {
+    void** items;     /* Array of pointers to items */
+    unsigned int capacity;    /* Maximum number of items */
+    unsigned int size;        /* Current number of items */
+    unsigned int item_size;   /* Size of each item in bytes */
+} Stack;
+
+/* Stack functions */
+
+/**
+* Initializes a new stack.
+* 
+* @param stack The stack to initialize.
+* @param item_size The size of each item in bytes.
+* @param initial_capacity The initial capacity of the stack.
+*/
+void stack_init(Stack* stack, unsigned int item_size, unsigned int initial_capacity);
+
+/**
+* Destroys the stack and frees allocated memory.
+* 
+* @param stack The stack to destroy.
+*/
+void stack_destroy(Stack* stack);
+
+/**
+* Pushes an item onto the stack.
+* 
+* @param stack The stack to push the item onto.
+* @param item The item to push (pointer to the item).
+* @return true if the item was successfully pushed, false if there was an error.
+*/
+bool stack_push(Stack* stack, void* item);
+
+/**
+* Pops the top item from the stack.
+* 
+* @param stack The stack to pop the item from.
+* @return A pointer to the popped item, or NULL if the stack is empty.
+*/
+void* stack_pop(Stack* stack);
+
+/**
+* Checks if the stack is empty.
+* 
+* @param stack The stack to check.
+* @return true if the stack is empty, false otherwise.
+*/
+bool stack_empty(const Stack* stack);
+
+/**
+* Returns the current size of the stack.
+* 
+* @param stack The stack to check.
+* @return The current number of items in the stack.
+*/
+unsigned int stack_size(const Stack* stack);
