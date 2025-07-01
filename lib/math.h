7ded7e36c27b22192d80f84214522fa51ab09524
salmon <elis.staaf@proton.me>
2025-07-02 00:58:09 +0200

    feat: (minor) add a synonym inf macro

--- lib/math.h
+++ lib/math.h
@@ -15,7 +15,8 @@
 double divide(double x, double y);
 
 /* Defines */
-#define INF divide(10, 0)
+#define INF      divide(10, 0)
+#define INFINITY divide(10, 0)
 
 /* Functions */
 double pow(double base, double exponent);
