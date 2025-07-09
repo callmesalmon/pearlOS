e452d1ae4c22b655fc2d9cd10edc7dc5ce0ea4e9
salmon <elis.staaf@proton.me>
2025-07-09 21:47:24 +0200

    feat: Update the outdated messages.h file

--- kernel/messages.h
+++ kernel/messages.h
@@ -11,6 +11,10 @@
 /* == Message naming conventions ==
  * 1. Structure
  *  LABEL_TYPE_MESSAGE
+ *  See (2) for info on labels,
+ *      (3) for info on types.
+ *  The messages should be clear
+ *  and concise.
  * 2. Labels
  *  KERNEL, FIRMWARE,...
  * 3. Types
@@ -20,6 +24,9 @@
  *  PANIC    fatal problem
  */
 
+// These first four functions do not require any "\n" appending,
+// as they are called with <kernel/kmsg.h> functions, all of which
+// append a "\n" automatically to the text.
 #define KERNEL_INFO_ENTERED       "Booting..."
 #define KERNEL_INFO_INIT_START    "Initializing..."
 #define KERNEL_INFO_INIT_DONE     "Done!"
@@ -38,7 +45,9 @@
 "|__/                                                            \n"
 #define KERNEL_INFO_MANUAL_HELP "Type \"help\" to open ksh manual.\n"
 #define KERNEL_INFO_SHELL_UNKNOWN_COMMAND   "Invalid command!\n"
-#define KERNEL_PANIC_MEMORY_INDEX_FULL      "Kernel index is full!\nCAUSE: Too many kmalloc() calls...\n"
-#define KERNEL_PANIC_MEMORY_FULL            "Kernel memory is full!\nCAUSE: The system ran out of RAM..."
+#define KERNEL_PANIC_MEMORY_INDEX_FULL      "Kernel index is full!\n" \
+                                            "CAUSE: Too many kmalloc() calls...\n"
+#define KERNEL_PANIC_MEMORY_FULL            "Kernel memory is full!\n" \
+                                            "CAUSE: The system ran out of RAM..."
 #define FIRMWARE_ERROR_ISR_EXCEPTION        "Recieved ISR exception: "
 #define FIRMWARE_ERROR_SMBIOS_ENTRY_MISSING "Recieved exception: Couldn't find an SMBIOS entry...\n"
