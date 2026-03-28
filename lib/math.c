22f1e8fda98bf6efd9ddd0dd3c66e67b6ff756d8
salmon <staaf.elis@gmail.com>
2026-03-28 13:14:39 +0100

    remove worthless ``divide()`` function

--- lib/math.c
+++ lib/math.c
@@ -1,16 +1,5 @@
 #include <math.h>
 
-/* x/y (x/y for y = 0: inf).
- * This way of accquiring infinity was
- * explored in my blog post, for more
- * info, see:
- *
- *     https://callmesalmon.github.io/ramblings/2025/01/31/infinity.html
- */
-double divide(double x, double y) {
-    return x / y;
-}
-
 /* do (1 *= base) * exponent */
 double pow(double base, double exponent) {
   double result = 1;
--- lib/math.h
+++ lib/math.h
@@ -1,14 +1,8 @@
 #pragma once
 
-/* No external libs needed. */
-
-/* We need this for the ``INF`` macro,
- * because x/0 = inf. DO NOT REMOVE. */
-double divide(double x, double y);
-
 /* Defines */
-#define INF      divide(10, 0)
-#define INFINITY divide(10, 0)
+#define INF      (10 / 0)
+#define INFINITY (10 / 0)
 
 /* Functions */
 double pow(double base, double exponent);
